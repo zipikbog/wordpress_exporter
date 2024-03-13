b4ec5312f9806f4a0929feda770f62ce2f7c20d2
zipikbog <70838873+zipikbog@users.noreply.github.com>
2024-03-13 12:54:15 +0300

    Update wordpress_exporter.go

--- wordpress_exporter.go
+++ wordpress_exporter.go
@@ -3,7 +3,7 @@ package main
 import (
     "net/http"
 
-    log "github.com/Sirupsen/logrus"
+    log "github.com/sirupsen/logrus"
     "github.com/prometheus/client_golang/prometheus/promhttp"
     "github.com/prometheus/client_golang/prometheus"
 
